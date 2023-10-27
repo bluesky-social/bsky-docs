11c2f871519ae1517495f44bab12920248ae8e94
Emily <emilyliu7321@gmail.com>
2023-10-26 22:10:37 -0700

    Add sample blocks blogpost

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/bluesky-social/bluesky-docs/tree/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
