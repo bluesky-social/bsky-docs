6c91c08bb58a01f32063d574b3a5b3e78c3a9fe2
emilyliu7321 <emilyliu7321@gmail.com>
2023-12-15 14:02:08 -0800

    Add mkdir step

--- atproto-openapi-types/scripts/get-lexicons.sh
+++ atproto-openapi-types/scripts/get-lexicons.sh
@@ -7,6 +7,7 @@ TAR_PATH="atproto-main/lexicons"
 
 # Delete existing files in the destination folder to remove out-of-date lexicons
 rm -rf $DEST_PATH/*
+mkdir $DEST_PATH
 
 # Download and extract the specified path
 curl $URL | tar -xz --strip=2 --directory $DEST_PATH $TAR_PATH
