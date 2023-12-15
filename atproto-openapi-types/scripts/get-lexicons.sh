0401079f2134dc22de72b071870b066be3a1cb22
emilyliu7321 <emilyliu7321@gmail.com>
2023-12-15 13:58:19 -0800

    Remove commit step

--- atproto-openapi-types/scripts/get-lexicons.sh
+++ atproto-openapi-types/scripts/get-lexicons.sh
@@ -2,7 +2,7 @@
 
 URL="https://codeload.github.com/bluesky-social/atproto/tar.gz/main"
 
-DEST_PATH="./atproto-openapi-types/lexicons"
+DEST_PATH="atproto-openapi-types/lexicons"
 TAR_PATH="atproto-main/lexicons"
 
 # Delete existing files in the destination folder to remove out-of-date lexicons
