cd3800f8846f70ce4cf12355d044f93ab216191c
emilyliu7321 <emilyliu7321@gmail.com>
2023-12-15 13:10:53 -0800

    Remove existing files in dest folder for out-of-date lexicons

--- atproto-openapi-types/scripts/get-lexicons.sh
+++ atproto-openapi-types/scripts/get-lexicons.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# TODO: Delete existing repo to remove stale files
 
 URL="https://codeload.github.com/bluesky-social/atproto/tar.gz/main"
 
-DEST_PATH="lexicon-to-openapi/lexicons"
+DEST_PATH="atproto-openapi-types/lexicons"
 TAR_PATH="atproto-main/lexicons"
+
+# Delete existing files in the destination folder to remove out-of-date lexicons
+rm -rf $DEST_PATH/*
 
 # Download and extract the specified path
 curl $URL | tar -xz --strip=2 --directory $DEST_PATH $TAR_PATH
