857347296bbd799b0117128092c07eeb9644ef5c
Paul Frazee <pfrazee@gmail.com>
2024-01-08 23:23:07 -0800

    Rework the homepage hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,51 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from 'react'
+import clsx from 'clsx'
+import Link from '@docusaurus/Link'
+import Image from '@theme/IdealImage'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
+import Layout from '@theme/Layout'
+import HomepageFeatures from '@site/src/components/HomepageFeatures'
+const Logo = require('@site/static/img/logo.svg').default
 
-import styles from './index.module.css';
+import styles from './index.module.css'
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={styles.heroBanner}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <Logo className={styles.heroImage} role="image" />
+        <h1 className={clsx('hero__title', styles.heroText)}>
+          Bluesky Developer APIs
+        </h1>
+        <p className={clsx('hero__subtitle', styles.heroText)}>
+          Build apps, bots, and feed generators on the Bluesky open social
+          network.
+        </p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
-            to="/docs/category/starter-templates">
-            Starter Templates
+            className={clsx('button', styles.heroButton)}
+            to="/docs/category/starter-templates"
+          >
+            Get Started
           </Link>
         </div>
       </div>
     </header>
-  );
+  )
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext()
   return (
     <Layout
       title="Bluesky Documentation"
-      description="Explore guides and tutorials to the Bluesky API.">
+      description="Explore guides and tutorials to the Bluesky API."
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
     </Layout>
-  );
+  )
 }
