8e983ee3ca9f200d563e7bdd2c0c3b16c6c6461c
emilyliu7321 <emilyliu7321@gmail.com>
2024-02-02 14:30:05 -0800

    update production url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://www.docs.bsky.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
