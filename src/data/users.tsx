f27c413f711e6d8b2546a31d441d452677cd955b
シュンジョーァ <68307970+mszpro@users.noreply.github.com>
2024-02-08 15:14:13 +0900

    Add Sora to community showcase
    Sora is an iOS and VisionOS app supporting Bluesky.

--- src/data/users.tsx
+++ src/data/users.tsx
@@ -440,6 +440,14 @@ const Users: User[] = [
     tags: ['client'],
   },
   {
+    title: 'Sora',
+    description: 'iOS and VisionOS client for Bluesky',
+    preview: require('./showcase/example-1.png'),
+    website: 'https://mszpro.com/sorasns',
+    author: 'https://bsky.app/profile/did:plc:fynelmajwyd4arrdpmm2mkqu',
+    tags: ['client'],
+  },
+  {
     title: 'SkyBridge',
     description: 'A bridge to use Mastodon apps with Bluesky',
     preview: require('./showcase/example-1.png'),
